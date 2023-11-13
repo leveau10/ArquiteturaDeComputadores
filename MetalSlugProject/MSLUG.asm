56c3b663c48d5cc4f88af7dcedc35ac3ca422e82
Leonardo Viana <76445679+leveau10@users.noreply.github.com>
2023-11-13 10:26:56 -0300

    new

--- MetalSlugProject/MSLUG.asm
+++ MetalSlugProject/MSLUG.asm
@@ -8,6 +8,7 @@ main:
 	jal arvoresPrim
 	jal arvoresSec
 	jal arvoresTer
+	jal personagem
 	
 fim:
 	addi $2 $0 10
@@ -18,28 +19,56 @@ ceu:  #CÉU
 	addi $5 $0 0x000791ff
 	add $8 $0 $0
 forCeu:
-	beq $8 8192 ceu2
+	beq $8 8192 nuvem
 	
 	sw $5 0($9)
 	addi $9 $9 4
 	addi $8 $8 1
 	j forCeu
+
+nuvem:
+	lui $9 0x1001
+	addi $9 $9 2096
+	add $8 $0 $0
+	add $10 $0 $0
+	addi $5 $0 0x00ff0000
+forNuvem:
+	beq $8 3 nuvemCima
+	
+	sw $5 0($9)
+	sw $5 4($9)
+	sw $5 8($9)
+	sw $5 12($9)
+	sw $5 16($9)
+	sw $5 20($9)
+	addi $8 $8 1
+	j forNuvem
+nuvemCima:
+	
 ceu2:
 	jr $31
 terra:   lui $9, 0x1001
 		add $9 $9 24576 #terra no ultimo quarto 
         	add $8, $0, $0
-       		addi $5, $0, 0x006c3c0c #cor marrom
-forTerra:   beq $8, 2048, terra2
-
+       		addi $5, $0, 0x0027661d #cor marrom
+forTerra:   beq $8, 256, terra2
+		
         
         	sw $5, 0($9)
         	addi $9, $9, 4
         	addi $8, $8, 1
         	j forTerra
 terra2:
+	addi $5 $0 0x006c3c0c
+forTerra2:
+
+	beq $8 2048 terra3
+	sw $5 0($9)
+	addi $9 $9 4
+	addi $8 $8 1
+	j forTerra2
+terra3:
 	jr $31
-	
 arvoresPrim:   #PRIMEIRA ÁRVORE
 	add $10 $0 $0 
 	lui $9 0x1001
@@ -74,12 +103,8 @@ forFolhagem:
 	addi $10 $10 1
 	j forFolhagem
 	
-	
-	
 arvoresPrim3:
 	jr $31
-
-
 
 arvoresSec: #SEGUNDA ÁRVORE
 	add $10 $0 $0
@@ -124,7 +149,7 @@ arvoresTer: ###TERCEIRA ÁRVORE
 	addi $9 $9 24428
 	addi $5 $0 0x00342b29
 forArvoresTer:   
-	beq $10 14 folhagem3
+	beq $10 20 folhagem3
 
 	sw $5 0($9)
 	sw $5 4($9)
@@ -152,4 +177,13 @@ forFolhagem3:
 	addi $10 $10 1
 	j forFolhagem3
 arvoresTer3:
+	jr $31
+
+personagem:
+	lui $9 0x1001
+	addi $5 $0 0x00000000
+	sw $5 24068($9)
+	sw $5 24072($9)
+	sw $5 24080($9)
+	sw $5 24084($9)
 	jr $31
